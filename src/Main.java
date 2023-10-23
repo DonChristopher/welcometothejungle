30058a93d6e3b0f3d94ebe009cb93d02ca1e03bf
super <csimonetro23dw@ikzubirimanteo.com>
2023-10-23 12:26:23 +0200

    algo

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+import java.util
+public class Main {
+    public static void main(String[] args) {
+
+        //con el scanner cogeis un numero entero positivo y una cadena
+
+
+        //Ver si el string es palomo
+
+        //Ver si el numero es mi hermano
+
+        //Ver cuantas mayus tiene el grillete
+
+        //Si no es ja primo factorizarlo chacho
+
+
+        //Mostrar el tamaño de la cadena perpetua
+
+
+
+    }
+}

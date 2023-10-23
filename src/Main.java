711967096d7f06479690b02fe55086822ee41d62
JaimeManero <jmaneromo23de@ikzubirimanteo.com>
2023-10-23 13:01:30 +0200

    ver si es numero primo. :P

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
         int numero=scanner.nextInt();
 
         System.out.println("Introduce una super cadena:");
-        String numero=scanner.nextLine();
 
 
 
@@ -18,6 +17,34 @@ public class Main {
         //Ver si el string es palomo
 
         //Ver si el numero es mi hermano
+        boolean esPrimo = esNumeroPrimo(numero);
+
+        if (esPrimo) {
+            System.out.println(numero + " es un número primo.");
+        } else {
+            System.out.println(numero + " no es un número primo.");
+        }
+    }
+
+    public static boolean esNumeroPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+        if (numero <= 3) {
+            return true;
+        }
+        if (numero % 2 == 0 || numero % 3 == 0) {
+            return false;
+        }
+
+        for (int i = 5; i * i <= numero; i += 6) {
+            if (numero % i == 0 || numero % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+
 
         //Ver cuantas mayus tiene el grillete
 
